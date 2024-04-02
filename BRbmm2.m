4e9d0052659ef5d30435fdeccbcca86e4e4f1528
WJXI <165768591+WJXI@users.noreply.github.com>
2024-04-02 16:49:37 +0800

    Add files via upload

--- BRbmm2.m
+++ BRbmm2.m
@@ -15,20 +15,6 @@ else
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 if N_V==4 % to modify
-    Rbmm=zeros(target_V*2,origin_V*2);
-    for i=1:origin_V
-        caci=i-1;
-        i_two=[fix(caci/4) fix((caci-4*fix(caci/4))/2) caci-4*fix(caci/4)-2*fix((caci-4*fix(caci/4))/2)];
-        for j=1:target_V
-            cacj=j-1;
-            j_two=[fix(cacj/4) fix((cacj-4*fix(cacj/4))/2) cacj-4*fix(cacj/4)-2*fix((cacj-4*fix(cacj/4))/2)];
-            for k=1:2
-                new_i=[i_two(1:2) k-1 i_two(3)]*[8 4 2 1]';
-                new_j=[j_two(1:2) k-1 j_two(3)]*[8 4 2 1]';
-                Rbmm(new_j+1,new_i+1)=Retraction(j,i);
-            end
-        end
-    end
     Rbmm=kron(eye(target_V/2),exchange)*kron(Retraction,eye(2))*kron(eye(origin_V/2),exchange);
 elseif N_V==2 && comp_state(3)==0 % 3 config
     Rbmm=kron(Retraction,eye(2));
